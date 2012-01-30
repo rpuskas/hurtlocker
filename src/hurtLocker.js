8c76d683671111f1637dc2fcf1659cad961176cb
Ryan Puskas <rpuskaspublic@gmail.com>
2012-01-30 00:15:00 -0600

    writing pseudocode to solve minesweeper

--- src/hurtLocker.js
+++ src/hurtLocker.js
@@ -1,61 +1,109 @@
 (function(){
-
-HL = {};
-HL.click_cell = function(row,column) {	
-	HL.cell_locator(row,column).trigger({
-		type: 'mouseup',
-		which: 1
-	});
-};
-
-HL.cell_locator = function(row,column){
-	return $("#g1" + "r" + row + "c" + column);
-}
-
-HL.refresh_minefield = function(){
-	HL.create_minefield(
-		$('tr.field').length,
-		$('tr.field:first').find('td').length,
-		HL.cell_locator);
-};
-
-HL.create_minefield = function(rows, columns,cell_locator){
 	
-	var minefield = Array(rows);
-	for(var x = 0;x<rows;x++){
-		minefield[x] = Array(columns)
-		for(var y = 0;y<columns;y++){
-			minefield[x][y] = cell_locator(x,y)
+//MineField should be able to be solved without any need from the dom
+MineField = function(data){
+
+	var _data = data;
+	var mark_remaining = function(){
+		
+	}
+	
+	grid_reduce = function(action){
+
+		// for(row = 0;row < HL.grid_data.rows; row++){
+		// 	for(column = 0; column < HL.grid_data.columns; column++){
+		// 		action(total,HL.cell_locator(row,column))			
+		// 	}
+		// }
+
+	}
+	
+	return {
+		
+		//Public API
+		next_click: function(){
+			//for all cells get min probability
+			//if there is a tie, pick one closest to middle (hot to get middle)
+			//return {x:'x-val',y:'y-val'};
+		},
+		
+		evaluate_all_cells: function(){
+			//for all cells left to right, top to bottom evaluate_bomb_likelyhood
+			//evaluate_bomb_likelyhood(x,y)
+		},
+		
+		random_click_probability: function(){
+			//estimated mines remaining div unclicked squares
+		},
+		
+		evaluate_bomb_likelyhood: function(x,y){
+		
+			//evaluate the likelyhood of a bomb being on a square
+			//it will need to feed this into evolving grids structure
+			
+			//return [cell value] / [unclicked neighbors]
+			
 		}
 	}
-	return minefield;
-
-};
-
-HL.mines_remaining = function(){
-	return parseInt($('.minesRemaining').attr('title'));
 }
 
-HL.add_if_unclicked = function(total,cell){
-	if(cell.attr('class') === 'unclicked'){
-		total++;
+DomAdaper = (function(){
+	
+	var cell_locator = function(row,column){
+		return $("#g1" + "r" + row + "c" + column).attr('class');
 	}
-}
-
-HL.grid_reduce = function(action){
-	var total;
-	for(row = 0;row < HL.grid_data.rows; row++){
-		for(column = 0; column < HL.grid_data.columns; column++){
-			action(total,HL.cell_locator(row,column))			
+	
+	var cell_value = function(value){
+		if(value == 'marked' || value == 'unclicked'){
+			return value
 		}
+		return value.replace('mines','');
 	}
-	return total;	
-}
+	
+	var create_minefield = function(rows, columns,cell_locator){
+
+		var minefield = Array(rows);
+		for(var x = 0;x<rows;x++){
+			minefield[x] = Array(columns)
+			for(var y = 0;y<columns;y++){
+				minefield[x][y] = HL.cell_value(cell_locator(x,y));
+			}
+		}
+		return minefield;
+	}
+	
+	return{
+				
+		refresh_minefield = function(){
+			return HL.create_minefield(
+				$('tr.field').length,
+				$('tr.field:first').find('td').length,
+				HL.cell_locator);
+		},
+		
+		click_cell = function(row,column) {	
+			HL.cell_locator(row,column).trigger({
+				type: 'mouseup',
+				which: 1
+			});
+		}
+				
+	}
+})();
 
 
 }());
 
 $(function(){
-	HL.click_cell(0,0);
-	HL.refresh_minefield();
+	
+	var run_until_solved = function(){
+
+		var minefield = new Minefield(HL.refresh_minefield())
+		HL.click_cell(minefield.next_click()) === 'clicked_mine' || return;
+		minefield.unclickedSquares === $('#g1minesRemaining').attr('title') && return;
+		
+		run_until_solved();
+		
+	}	
+	
 });
--- tests/hurtLocker_tests.js
+++ tests/hurtLocker_tests.js
@@ -2,10 +2,24 @@ describe('hurt_locker_tests', function(){
 		
 	it('create_minefield_should_create_data_structure', function() {
 		
-		var mine_field = HL.create_minefield(2,3,function(){return 'X'});
-		expect(mine_field.length).toEqual(2);
-		expect(mine_field[1].length).toEqual(3);
-		expect(mine_field[1][2]).toEqual('X');
+		spyOn(HL, 'cell_locator').andReturn('X');
+		var mine_field = HL.create_minefield(3,3,HL.cell_locator);
+		expect(HL.cell_locator.callCount).toEqual(3*3);
+		expect(mine_field[0][0]).toEqual('X');
+		expect(mine_field[2][2]).toEqual('X');
+		
+	});
+	
+	it('should_translate_cell_class_value_into_grid_value', function(){
+		
+		expect(HL.cell_value('unclicked')).toEqual('unclicked');
+		expect(HL.cell_value('mines1')).toEqual('1');
+		expect(HL.cell_value('mines2')).toEqual('2');
+		expect(HL.cell_value('mines3')).toEqual('3');
+		expect(HL.cell_value('mines4')).toEqual('4');
+		expect(HL.cell_value('mines5')).toEqual('5');
+		expect(HL.cell_value('mines6')).toEqual('6');
+		expect(HL.cell_value('marked')).toEqual('marked');
 		
 	});
 	
