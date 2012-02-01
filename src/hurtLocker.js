fdee1c8e32bf14f904bbcc55e79fb86bc4099ce3
Ryan Puskas <rpuskaspublic@gmail.com>
2012-02-01 16:38:14 -0600

    adding grid, mine, and dom mapping

--- src/hurtLocker.js
+++ src/hurtLocker.js
@@ -1,26 +1,13 @@
 (function(){
 	
 //MineField should be able to be solved without any need from the dom
-MineField = function(data){
+Minefield = function(data,bombs){
 
+	var _bombs = bombs;
 	var _data = data;
-	var mark_remaining = function(){
 		
-	}
-	
-	grid_reduce = function(action){
-
-		// for(row = 0;row < HL.grid_data.rows; row++){
-		// 	for(column = 0; column < HL.grid_data.columns; column++){
-		// 		action(total,HL.cell_locator(row,column))			
-		// 	}
-		// }
-
-	}
-	
 	return {
 		
-		//Public API
 		next_click: function(){
 			//for all cells get min probability
 			//if there is a tie, pick one closest to middle (hot to get middle)
@@ -32,56 +19,62 @@ MineField = function(data){
 			//evaluate_bomb_likelyhood(x,y)
 		},
 		
-		random_click_probability: function(){
-			//estimated mines remaining div unclicked squares
+		random_chance: function(){
+			var unclicked = Grid.reduce(_data,function(x,y){ return _data[x][y].state == 'unclicked'; })
+			return _bombs / unclicked;
 		},
 		
-		evaluate_bomb_likelyhood: function(x,y){
-		
-			//evaluate the likelyhood of a bomb being on a square
-			//it will need to feed this into evolving grids structure
+		set_neighbors_chance: function(x,y){
 			
-			//return [cell value] / [unclicked neighbors]
+			var neighbors = Grid.neighbors(_data,x,y);
+			var home = _data[x][y];
 			
+			var unclicked_neighbors = _.filter(neighbors,function(neighbor){ return neighbor.state === 'unclicked' });
+			
+			for(var i = 0; i < unclicked_neighbors.length; i++){
+				unclicked_neighbors[i].chance = home.state / unclicked_neighbors.length;
+			}
 		}
 	}
 }
 
-DomAdaper = (function(){
+MineCell = function(state,chance){
+	return{
+		state: state,
+		chance: chance
+	}
+}
+
+HL = (function(){
 	
 	var cell_locator = function(row,column){
 		return $("#g1" + "r" + row + "c" + column).attr('class');
 	}
 	
-	var cell_value = function(value){
-		if(value == 'marked' || value == 'unclicked'){
-			return value
-		}
-		return value.replace('mines','');
-	}
-	
-	var create_minefield = function(rows, columns,cell_locator){
-
-		var minefield = Array(rows);
-		for(var x = 0;x<rows;x++){
-			minefield[x] = Array(columns)
-			for(var y = 0;y<columns;y++){
-				minefield[x][y] = HL.cell_value(cell_locator(x,y));
-			}
-		}
-		return minefield;
+	var cell_value_locator = function(row,column){
+		return cell_value($("#g1" + "r" + row + "c" + column).attr('class'));
 	}
 	
 	return{
+		
+		cell_value: function(value){
+			
+			if(value == 'marked' || value == 'unclicked'){}
+			else{
+				value = value.replace('mines','');
+			}
+
+			return MineCell(value,100)
+		},
 				
-		refresh_minefield = function(){
-			return HL.create_minefield(
+		refresh: function(){
+			return Grid.create(
 				$('tr.field').length,
 				$('tr.field:first').find('td').length,
-				HL.cell_locator);
+				cell_value_locator);
 		},
 		
-		click_cell = function(row,column) {	
+		click_cell : function(row,column) {	
 			HL.cell_locator(row,column).trigger({
 				type: 'mouseup',
 				which: 1
@@ -91,19 +84,65 @@ DomAdaper = (function(){
 	}
 })();
 
+Grid = (function(){
+	
+	return{
+		
+		create : function(rows, columns,cell_value){
+			var minefield = Array(rows);
+			for(var x = 0;x<rows;x++){
+				minefield[x] = Array(columns)
+				for(var y = 0;y<columns;y++){
+					minefield[x][y] = cell_value(x,y);
+				}
+			}
+			return minefield;
+		},
+		
+		reduce : function(grid,criteria){
+			var total = 0;
+			for(row = 0;row < grid.length; row++){
+				for(column = 0; column < grid[row].length; column++){
+					if(criteria(row,column)){
+						total = total + 1;
+					}			
+				}
+			}
+			return total;
+		},
+		
+		neighbors : function(grid,x,y){
+			
+			var not_null = function(x,y){ return grid[column] && grid[column][row]}
+			var home_cell = function(column,row){ return (x == column && row == y) }
+			
+			var neighbors = [];
+			for(column = x-1; column<(x-1+3); column++){
+				for(row = y-1; row < (y-1+3) ; row++){
+					if(!home_cell(column,row) && not_null(column,row)){
+						neighbors.unshift(grid[column][row])
+					}
+				}
+			}
+			return neighbors;
+		}		
+	}
+}());
+
 
 }());
 
 $(function(){
 	
-	var run_until_solved = function(){
-
-		var minefield = new Minefield(HL.refresh_minefield())
-		HL.click_cell(minefield.next_click()) === 'clicked_mine' || return;
-		minefield.unclickedSquares === $('#g1minesRemaining').attr('title') && return;
-		
-		run_until_solved();
-		
-	}	
+	// var run_until_solved = function(){
+	// 
+	// 	var minefield = new Minefield(HL.refresh_minefield())
+	// 	
+	// 	HL.click_cell(minefield.next_click()) === 'clicked_mine' || return;
+	// 	minefield.unclickedSquares === $('#g1minesRemaining').attr('title') && return;
+	// 	
+	// 	run_until_solved();
+	// 	
+	// }	
 	
 });
--- tests/hurtLocker_tests.js
+++ tests/hurtLocker_tests.js
@@ -1,25 +1,85 @@
 describe('hurt_locker_tests', function(){
+			
+	it('should_translate_cell_class_value_into_grid_value', function(){
 		
-	it('create_minefield_should_create_data_structure', function() {
-		
-		spyOn(HL, 'cell_locator').andReturn('X');
-		var mine_field = HL.create_minefield(3,3,HL.cell_locator);
-		expect(HL.cell_locator.callCount).toEqual(3*3);
-		expect(mine_field[0][0]).toEqual('X');
-		expect(mine_field[2][2]).toEqual('X');
+		expect(HL.cell_value('unclicked').state).toEqual('unclicked');
+		expect(HL.cell_value('mines1').state).toEqual('1');
+		expect(HL.cell_value('mines2').state).toEqual('2');
+		expect(HL.cell_value('mines3').state).toEqual('3');
+		expect(HL.cell_value('mines4').state).toEqual('4');
+		expect(HL.cell_value('mines5').state).toEqual('5');
+		expect(HL.cell_value('mines6').state).toEqual('6');
+		expect(HL.cell_value('marked').state).toEqual('marked');
 		
 	});
 	
-	it('should_translate_cell_class_value_into_grid_value', function(){
+});
+
+Mother = (function(){
+	return{
+		unclicked: function(rows,columns){
+			return Grid.create(rows,columns,function(){ return {state:'unclicked',chance:100}})
+		}
+	}	
+	
+}());
+
+describe('grid_utility', function(){
+
+	it('should_create_grid_with_values_set_to_cell_values',function(){
+		var grid = Grid.create(3,3,function(x,y){return 9});
+		window.debug = grid;
+		expect(grid.length).toEqual(3);
+		expect(grid[0].length).toEqual(3);
+		expect(grid[0][0]).toEqual(9);
+	});
+	
+	it('should_get_neighboring_cells', function(){
+		grid = Mother.unclicked(3,3);
+		expect(Grid.neighbors(grid,1,1).length).toEqual(8);
+		expect(Grid.neighbors(grid,0,0).length).toEqual(3);
+		expect(Grid.neighbors(grid,0,1).length).toEqual(5);
 		
-		expect(HL.cell_value('unclicked')).toEqual('unclicked');
-		expect(HL.cell_value('mines1')).toEqual('1');
-		expect(HL.cell_value('mines2')).toEqual('2');
-		expect(HL.cell_value('mines3')).toEqual('3');
-		expect(HL.cell_value('mines4')).toEqual('4');
-		expect(HL.cell_value('mines5')).toEqual('5');
-		expect(HL.cell_value('mines6')).toEqual('6');
-		expect(HL.cell_value('marked')).toEqual('marked');
+		expect(Grid.neighbors(grid,0,1)[0].chance).toEqual(100);
+		expect(Grid.neighbors(grid,0,1)[0].state).toEqual('unclicked');
+	});
+	
+});
+
+describe('minefield',function(){
+	
+	it('should_calculate_random_square_probability', function(){
+		expect(Minefield(Mother.unclicked(10,10),10).random_chance()).toEqual(0.1);
+		expect(Minefield(Mother.unclicked(10,10),50).random_chance()).toEqual(0.5);
+	});
+	
+	describe('when_a_cell_indicates_surrounding_mine_count',function(){
+					
+		var grid;
+		var minefield;
+		
+		beforeEach(function(){
+			grid = Mother.unclicked(3,3);
+			minefield = Minefield(grid,1);
+		});
+		
+		it('should_set_probability_of_neighbors', function(){
+
+			grid[1][1] = MineCell(1,0);
+			minefield.set_neighbors_chance(1,1);
+			expect(grid[0][0].chance).toEqual(1/8);
+		});
+		
+		it('should_set_probability_of_neighbors',function(){
+		
+		// it('should_not_overwite_a_lesser_probability_cell',function(){
+		// 	
+		// 	grid[0][0] = MineCell(1,1/8);
+		// 	grid[1][1] = MineCell(2,0);
+		// 	
+		// 	minefield.set_neighbors_chance(1,1);			
+		// 	expect(grid[0][0].chance).toEqual(2/8);
+		});
 		
 	});
 	
